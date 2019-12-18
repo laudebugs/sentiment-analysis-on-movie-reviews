1dae4daa3ecd6de78f83cb6bb33422213dfb21f8
lbugasu <lbugasu@gmail.com>
2019-12-18 00:11:39 -0500

    modified NB to run on entire training file and produce correct output to submit to Kaggle. Also edited the Readme file.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-In order to classify sentences on a five point scale following a challenge on Kaggle: https://www.kaggle.com/c/sentiment-analysis-on-movie-reviews, 
-we are developing a sentiment analysis classifier to classify the sentiment in the text for each phrases. 
+In order to classify sentences on a five point scale following a challenge on Kaggle: https://www.kaggle.com/c/sentiment-analysis-on-movie-reviews,
+we are developing a sentiment analysis classifier to classify the sentiment in the text for each phrases.
 
 The current Naive Bayes classifier, found in the baseline directory achieves a simple classification based on whether or not a words with a particular sentiment appears in the sentence
 
-To run the program, simply call the program: python baselineNB.py
+To run the program, simply call the program: python baselineNB.py test.tsv
+
 To evaluate, based on the evaluation algorithm we wrote, run, from the folder: evaluation: python evaluate_f_score.py test_set answer_key
-          for example, to run against the development set: python evaluate_f_score.py ../dataset/development.tsv ../dataset/devanskey.tsv
+          for example, to run against the development set: python evaluate_f_score.py ../outputs/NB_file_output.tsv ../dataset/devanskey.tsv
--- baselines/modNB.py
+++ baselines/modNB.py
@@ -9,12 +9,12 @@ from __future__ import division
 import nltk
 import numpy as np
 import os
-import sys
+import re
 
 os.chdir("..")
 
 #import the training file
-training_file = open("dataset/train.tsv", "r")
+training_file = open("dataset/training.tsv", "r")
 training_file_lines = training_file.readlines()
 
 #The length of the training file
@@ -27,6 +27,53 @@ The features were implemented in the following order
     of a particular sentiment over all words that occur as instances of that particular sentiment.
 
 """
+
+class Node:
+    def __init__(self):
+        self.phrase = None
+        self.sentiment = None
+        self.right = None
+        self.left = None
+
+def branch(list_phrases, i, k, root):
+    #print list_phrases[k][4]
+
+    # The base case where we have finished parsing the entire sentence
+    # OR
+    # We have reached the mentioned parsed phrases
+    if (len(list_phrases[k][4])-1)-i==1 or k==(len(list_phrases)-1):
+        # Assign values of the leaf node
+        root.phrase = list_phrases[k][2]
+        root.sentiment = list_phrases[k][3]
+        i+=1;
+        k+=1;
+        return
+
+        # In the case where there are phrases that are not listed in the dataset and therefore skipped
+    elif list_phrases[0][4][i]!=list_phrases[k][4][0]:
+        # Assign the leaf node
+        root.phrase = list_phrases[k][2]
+        root.sentiment = list_phrases[k][3]
+        print(list_phrases[0][4])
+        i = int(list_phrases[0][4].index(list_phrases[k][4][0]))+1
+        k+=1
+        return
+
+    root.left = Node()
+    # Go Left
+
+    # Assign the root node
+    k+=1
+    branch(list_phrases, i, k, root.left)
+    root.phrase = list_phrases[k][2]
+    root.sentiment = list_phrases[k][3]
+    root.right = Node()
+
+    j = i+len(list_phrases[k][4])-1
+
+    # Go Right
+    branch(list_phrases, j, k, root.right)
+
 # A dictionary that will hold key value pairs with keys being a word that occurs in all the documents
 #  and values being a list [0 for k in range(5)] of the number of instances the word appears as an instance of class i, such that 0<=i<k
 
@@ -39,24 +86,45 @@ logprior = [ 0 for i in range (5)]
 # loglikelihood is a dictionary of all the words in the vocabulary representing the probabilities that a word occurs as an instance of each of the classes
 loglikelihood = {}
 
+parsed_sentences = []
+sentence_parse = []
+sentenceID = ''
 # Loop throughout the entire training data.
-header_row = training_file_lines[0]
-for i in range(1, len_train):
+for i in range(len_train):
     # Split the tab separated values into a list so that we can isolate the data on each line.
     # The data is in the following format: [PhraseId, SentenceId, Phrase, Sentiment]
-    values = training_file_lines[i].split("\t")
-    phrase = values[2]
-    sentiment = int(values[3])
+    phrase_values = training_file_lines[i].split("\t")
+    #Add a tokenized form of the phrase to the end of the phrase_values
+    phrase_values.append(re.split("\s+", phrase_values[2]))
+
+    #if this is the first phrase to be parsed
+    if i==0:
+        sentenceID = phrase_values[1]
+        sentence_parse.append(phrase_values)
+    # if the sentence ID changes or you are at the end of the file,
+    # Append the parsed sentences to the list of sentences
+    if phrase_values[1]!=sentenceID or i==len_train-1:
+        # Append the parsed sentences to the list of sentences
+        parsed_sentences.append(sentence_parse)
+        # Initialize the list parsed sentence
+        sentence_parse = []
+        # Assign the current sentence ID to the next sentence
+        sentenceID = phrase_values[1]
+    # Add this phrase to the parsed sentences
+    sentence_parse.append(phrase_values)
+
+
+
+    """
     # Tokenize the phrase
     all_words = nltk.word_tokenize(phrase)
     words = []
     #Cap no of words instances in a document at 1, i.e. do not include repeated words
-    #Also handle negation
     for word in all_words:
         if word not in words:
             words.append(word)
     # Analyse the phrase
-    """
+
     Note: This analysis assumes that for each phrase that indicates a particular sentiment, all the words in the phrase will be marked as bearing that sentiment.
     An example would be: for words that are neutral, they may be marked as bearing a sentiment of negative or positive, such as
     One approach would be analysing the single words, then moving on up the tree. However, assuming that all the data is parsed correctly, there would be no new
@@ -64,7 +132,7 @@ for i in range(1, len_train):
 
 
 
-    """
+
     for w in words:
         # If the word was not previously in the Vocabulary
         if w not in V:
@@ -74,9 +142,14 @@ for i in range(1, len_train):
         classcounts[sentiment]+=1
         # Increment the number of times word w appears as an instance of sentiment x
         V[w][sentiment]+=1
-
+    """
     # Done looping through training data
 
+Treebank = []
+for i in range(len(parsed_sentences)):
+    tree = Node()
+    branch(parsed_sentences[i], 0, 0, tree)
+    Treebank.append(tree)
 
 # Calculate prior probabilities P(c): logprior
 for c in range(5):
@@ -93,23 +166,23 @@ for word in V:
 # Done training
 
 
-### Run the trainied program against the test file ###
+### Run the trainied program against the development file ###
 
-#Open the file to run against
-this_file = open("dataset/"+sys.argv[1], "r")
-this_file_lines = this_file.readlines()
+#Open the development file
+dev_file = open("dataset/development.tsv", "r")
+dev_file_lines = dev_file.readlines()
 
 # Initialize a variable to hold the output
-this_output = "PhraseId,Sentiment\n"
+dev_output = ""
 # Loop through the lines and assign sentiments on the go.
-for l in range(1, len(this_file_lines)):
+for line in dev_file_lines:
     # Initialize the probabilities of all the classes for each particular phrase.
     sum = [ 0 for i in range (5)]
     # Add the logprior probabilities
     for i in range(5):
         sum[i] += logprior[i]
     # Split the data, the phrase will be contained at the index 2
-    values = this_file_lines[l].split("\t")
+    values = line.split("\t")
     phrase = values[2]
     tokens = nltk.word_tokenize(phrase)
     #Loop through the tokens
@@ -121,14 +194,14 @@ for l in range(1, len(this_file_lines)):
                 sum[c] += V[word][c]
 
     #Add the assigned sentiment to the output file in the format: PhraseId, Sentiment
-    this_output += values[0] + "," + str(sum.index(max(sum))) + "\n"
+    dev_output += values[0] + "\t" + str(sum.index(max(sum))) + "\n"
 
 # Print the output to a file.
-NB_file_out = open("outputs/NB_file_output.csv", "w+")
-NB_file_out.write(this_output)
+NB_file_out = open("outputs/NB_file_output.tsv", "w+")
+NB_file_out.write(dev_output)
 
 
 # Close the files
 training_file.close()
-this_file.close()
+dev_file.close()
 NB_file_out.close()
